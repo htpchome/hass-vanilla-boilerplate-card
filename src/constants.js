f29dc8f5c54ae5a194526664422c63d4ee517bd8
htpchome <117685139+htpchome@users.noreply.github.com>
2026-07-30 17:43:32 -0500

    36

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@
  */
 
 // ---------- Card identity ----------
-export const CARD_VERSION = "0.1.35";
+export const CARD_VERSION = "0.1.36";
 export const CARD_TYPE = "hass-vanilla-boilerplate-card";
 export const CARD_NAME = "HASS Vanilla Boilerplate Card";
 export const CARD_DESCRIPTION =
