4017c2d946299118e139599b3ff8a53042ffc4d0
htpchome <117685139+htpchome@users.noreply.github.com>
2026-07-30 15:38:08 -0500

    32

--- hass-vanilla-boilerplate-card.js
+++ hass-vanilla-boilerplate-card.js
@@ -11,7 +11,7 @@
    */
 
   // ---------- Card identity ----------
-  const CARD_VERSION = "0.1.31";
+  const CARD_VERSION = "0.1.32";
   const CARD_TYPE = "hass-vanilla-boilerplate-card";
   const CARD_NAME = "HASS Vanilla Boilerplate Card";
   const CARD_DESCRIPTION =
